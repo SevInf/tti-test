e433a65e2c98bccefb81c51efea70d214db83fcf
Sergey Tatarintsev <sergey.tatarintsev@researchgate.net>
2020-02-10 16:37:24 +0100

    Convert to now.sh app

--- api/example.js
+++ api/example.js
@@ -1,12 +1,11 @@
 'use strict';
-const http = require('http');
 
-http.createServer((req, res) => {
+module.exports = function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
     res.write(' ');
     setTimeout(() => {
         res.end('<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Hello</h1></body></html>');
-    }, 15000);
-}).listen(8080);
+    }, 5000);
+};
